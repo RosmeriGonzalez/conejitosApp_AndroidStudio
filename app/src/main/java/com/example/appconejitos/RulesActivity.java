02386d62eb6930921a73a629f193624ce133792a
RosmeriGonzalez <rosmerigonzalez45@yahoo.com>
2025-09-09 17:13:26 -0600

    Add start button in rules screen

--- app/src/main/java/com/example/appconejitos/RulesActivity.java
+++ app/src/main/java/com/example/appconejitos/RulesActivity.java
@@ -1,6 +1,8 @@
 package com.example.appconejitos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,13 @@ public class RulesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+
+        Button btnHome = findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(v -> {
+            Intent intent = new Intent(this, StartActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        });
     }
 }
